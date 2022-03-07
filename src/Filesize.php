5ac2685764cb06ac6906a7dc1a466305480d0f00
David van der Tuijn <david@digitalimpact.nl>
2022-03-07 21:06:48 +0100

    Code Optimization

--- src/Filesize.php
+++ src/Filesize.php
@@ -2,40 +2,43 @@
 
 namespace Davidvandertuijn;
 
+/**
+ * Filesize.
+ */
 class Filesize
 {
     /**
      * To Human Friendly.
      *
-     * @param int    $iBytes
-     * @param string $sForceUnit
-     * @param string $sFormat
-     * @param string $sStandard
+     * @param int $bytes
+     * @param string $forceUnit
+     * @param string $format
+     * @param string $standard
      *
      * @return string
      */
-    public static function toHumanFriendly(int $iBytes, ?string $sForceUnit = '', ?string $sFormat = '', ?string $sStandard = 'SI'): string
+    public static function toHumanFriendly(int $bytes, ?string $forceUnit = '', ?string $format = '', ?string $standard = 'SI'): string
     {
         // Format
-        $sFormat = empty($sFormat) ? '%01.2f %s' : (string) $sFormat;
+        $format = empty($format) ? '%01.2f %s' : (string) $format;
 
         // Standard
 
-        if ($sStandard == 'IEC'
-        || strpos($sForceUnit, 'i') !== false) {
-            $aUnits = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-            $iMod = 1024;
+        if ($standard == 'IEC'
+        || strpos($forceUnit, 'i') !== false) {
+            $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+            $mod = 1024;
         } else {
-            $aUnits = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-            $iMod = 1000;
+            $units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+            $mod = 1000;
         }
 
         // Determine Unit
 
-        if (($iPower = array_search((string) $sForceUnit, $aUnits)) === false) {
-            $iPower = ($iBytes > 0) ? floor(log($iBytes, $iMod)) : 0;
+        if (($power = array_search((string) $forceUnit, $units)) === false) {
+            $power = ($bytes > 0) ? floor(log($bytes, $mod)) : 0;
         }
 
-        return sprintf($sFormat, $iBytes / pow($iMod, $iPower), $aUnits[$iPower]);
+        return sprintf($format, $bytes / pow($mod, $power), $units[$power]);
     }
 }
